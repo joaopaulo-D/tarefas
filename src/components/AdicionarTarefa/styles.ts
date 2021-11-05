271244d6f4cb0a1c01c2a642b0a7e36a5598fb5d
João Paulo <81633820+joaopaulo-D@users.noreply.github.com>
2021-11-05 12:39:25 -0300

    Criado um styles.ts
    Estilo do index.tsx do componente Adicionar tarefa

---
+++ src/components/AdicionarTarefa/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    border: 1px solid white;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+`;
+
+export const Image = styled.div`
+    margin-right: 5px;
+    font-size: 25px;
+`;
+
+export const Input = styled.input`
+    outline: 0;
+    border: 0;
+    background: transparent;
+    color: #fff;
+    font-size: 18px;
+    flex: 1;
+`;
