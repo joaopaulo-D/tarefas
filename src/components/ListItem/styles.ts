79befc1c76879f957433d314421e0f549a9a7614
João Paulo <81633820+joaopaulo-D@users.noreply.github.com>
2021-11-05 12:40:23 -0300

    Update styles.ts

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,11 +1,22 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    display: flex;
-    background-color: #20212c;
-    padding: 10px;
-    align-items: center;
-    border-radius: 10px;
-    margin-bottom: 10px;
-    color: white;
-`;
+type PropsContainer = {
+    done: boolean;
+}
+
+export const Container = styled.div(({ done } : PropsContainer) => (
+    `
+        display: flex;
+        background-color: #20212c;
+        padding: 10px;
+        align-items: center;
+        border-radius: 10px;
+        margin-bottom: 10px;
+        color: white;
+
+        label {
+            color: #ccc;
+            text-decoration: ${done ? 'line-through' : 'initial'};
+        }
+    `
+));
