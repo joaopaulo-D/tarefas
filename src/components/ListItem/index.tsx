c01fbed9ecb744baf894854f133a81cbe28cc1a7
João Paulo <81633820+joaopaulo-D@users.noreply.github.com>
2021-11-05 12:35:43 -0300

    Adicionado o useState
    Foi adicionado o useState para verificar o estado do done da tarefa se ele for true (checkbox ativido) se for false (checkbox desativado)

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import * as S from "./styles";
 import { Item } from "../../types/Item";
 
@@ -6,9 +7,12 @@ type Props = {
 }
 
 const ListItem = ({ item } : Props) => {
+
+    const [isChecked, setIsChecked] = useState(item.done);
+
     return(
-        <S.Container>
-            <input type="checkbox" />
+        <S.Container done={isChecked}>
+            <input type="checkbox" checked={isChecked} onChange={e => setIsChecked(e.target.checked)}/>
             <label>{item.nome}</label>
         </S.Container>
     );
