c7384ac9dcd108f4e496a90d3117d93f8c1d9ae5
João Paulo <81633820+joaopaulo-D@users.noreply.github.com>
2021-11-05 12:37:16 -0300

    Adicionado o handleAdicionarTarefa
    foi adicionado a função handleAdicionarTarefa para adicionar a tarefa ao menu de listagem

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as S from "./App.styles";
 import { Item } from "./types/Item";
 import ListItem from "./components/ListItem";
+import AdiconarTarefa from "./components/AdicionarTarefa";
 
 const App = () => {
 
@@ -10,12 +11,23 @@ const App = () => {
     { id: 2, nome: 'Pragramar Amanha', done: false }
   ])
 
+  function handleAdicionarTarefa(nomeTarefa: string){
+      let newTarefa = [...list];
+      newTarefa.push({
+        id: list.length + 1,
+        nome: nomeTarefa,
+        done: false
+      })
+      setList(newTarefa);
+  } 
+
   return(
       <S.Container>
         <S.Area>
           <S.Header>Listagem de Tarefas</S.Header>
 
-          {/* Adicionar Tarefas */}
+          <AdiconarTarefa onEnter={handleAdicionarTarefa}/>
+
           {list.map((item, index) => (
               <ListItem key={index} item={item}/>
           ))}
